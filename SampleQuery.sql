c2f5d12f2456048f2c01eb23d3e9e5d20550bbd1
Erik McLaughlin <erikm6872@gmail.com>
2015-11-16 23:26:15 -0700

    Added a query

--- SampleQuery.sql
+++ SampleQuery.sql
@@ -2,5 +2,11 @@ USE Clery;
 
 SELECT CA.IncidentDescription LIMIT 5
     FROM CASE AS CA, CRIMES AS CR
-    WHERE CA.CID = CR.CrimeID 
-    AND CR.CName = 'DUI';
+    WHERE CA.CID=CR.CrimeID 
+    AND CR.CName='DUI';
+    
+SELECT D.Ddescription LIMIT 5
+    FROM DISPOSITION AS D, CASE AS C, LOCATION AS L
+    WHERE D.Disposition=C.Disposition
+    AND C.LocationID=L.LocationID
+    AND L.Name='South Fieldhouse Lot';
